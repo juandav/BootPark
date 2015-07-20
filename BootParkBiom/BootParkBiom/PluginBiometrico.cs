596684e7be3aedf3cbfef5ac49e25f0525bd5dd7
juandav <j.echeverry@udla.edu.co>
2015-07-20 09:52:40 -0500

    Agregado la Estructura para Plugin del Dispoitivo Biometrico.

---
+++ BootParkBiom/BootParkBiom/PluginBiometrico.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace BootParkBiom
+{
+    [ComVisibleAttribute(true)]
+    [Guid("37CEBA33-0279-4C01-B201-3CB52D3C778F")]
+    [ProgId("BootParkBiom.PluginBiometrico")]
+    public class PluginBiometrico
+    {
+        /// <summary>
+        ///     Función de prueba para verificar la conexión del plugin
+        /// </summary>
+        /// <returns>Texto</returns>
+        public string TextoPrueba()
+        {
+            return "Reponde desde el Plugin";
+        }
+
+    }
+}
